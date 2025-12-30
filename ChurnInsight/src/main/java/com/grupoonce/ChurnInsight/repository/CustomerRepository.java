c0d0ae039fcd99b2d5cfecb256cc53d4c95e3dfe
Brian <macielbrianexe@gmail.com>
2025-12-30 12:40:46 -0300

    Modificacion del Repository

---
+++ ChurnInsight/src/main/java/com/grupoonce/ChurnInsight/repository/CustomerRepository.java
@@ -0,0 +1,5 @@
+package com.grupoonce.ChurnInsight.repository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
--- ChurnInsight/src/main/java/com/grupoonce/ChurnInsight/repository/EntradaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupoonce.ChurnInsight.repository;
-
-import org.springframework.data.jpa.repository.Query;
-
-public interface EntradaRepository extends JpaRepository<Entrada, Long> {
-
-}
